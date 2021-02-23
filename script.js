3526319aae11d98540e2f037e07b81f006373499
soyqureshi <77929398+soyqureshi@users.noreply.github.com>
2021-02-24 03:27:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+
+
+function contactMe () {
+    var namess = document.getElementsByClassName("sqheadname");
+    namess[0].innerHTML = "+91 9167-553-218";
+
+    document.querySelector (".sqheadname").style.color = "chartreuse";
+    document.querySelector (".head").style.animation = "fro 20s linear infinite alternate";
+}
+
+
+
+// function contactMe () { 
+//     document.getElementById ("sqheadname").innerHTML = ("55555555555");
+
+//     document.querySelector ("#sqheadname").style.backgroundColor = "chartreuse";
+// }
+
+
+function hireMeCv () {
+    alert ("Please email me on shoaib.qsh@gmail.com for CV");
+
+}
+
+
+function workInPogress () {
+    document.getElementById ("wip").innerHTML = "Site Under Construction";
+    
+}
