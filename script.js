647f6b8d9822225cbea2a22d6dc23567b67aa4b1
soyqureshi <77929398+soyqureshi@users.noreply.github.com>
2021-02-26 00:22:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ function contactMe () {
     document.getElementById ("sqheadname").innerHTML = "+91 9167-553-218";
 
     document.querySelector ("#sqheadname").style.color = "chartreuse";
+    document.querySelector ("#sqheadname").style.animation = "sqfade 0.1s linear 25 alternate";
+    document.querySelector ("#sqheadname").style.fontSize = "0.9em";
     document.querySelector (".head").style.animation = "fro 20s linear infinite alternate";
 }
 
