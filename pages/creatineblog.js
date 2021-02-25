67cf78f85dd98efd19132f76fa6f1a70d7a6430e
soyqureshi <77929398+soyqureshi@users.noreply.github.com>
2021-02-25 23:17:31 +0530

    Add files via upload

---
+++ pages/creatineblog.js
@@ -0,0 +1,8 @@
+
+function buttonShadow() {
+
+    document.querySelector ("#buttt").style.boxShadow = "none";
+    document.querySelector ("#buttt").style.color = "white";
+    document.querySelector ("#buttt").style.backgroundColor = "black";
+
+}
