2309aa1be968b761487849e09c95e5b6bbbf47d7
soyqureshi <77929398+soyqureshi@users.noreply.github.com>
2021-02-28 16:05:12 +0530

    Add files via upload

---
+++ pages/bio.js
@@ -0,0 +1,52 @@
+function msOffice () {
+    document.getElementById ("msoffice").value = "5";
+    
+    
+}
+
+function photoShop () {
+    document.getElementById ("photoshop").value = "4";
+        
+}
+
+function illusTrator () {
+    document.getElementById ("Illustrator").value = "4";
+        
+}
+
+function sketchUp () {
+    document.getElementById ("sketchup").value = "4";
+        
+}
+
+function htMl () {
+    document.getElementById ("html").value = "3";
+        
+}
+
+function cSs () {
+    document.getElementById ("css").value = "2";
+        
+}
+
+function jaVa () {
+    document.getElementById ("javascript").value = "2";
+        
+}
+
+function skills () {
+    document.getElementById ("msoffice").value = "5";
+ 
+document.getElementById ("photoshop").value = "4";
+
+document.getElementById ("Illustrator").value = "4"
+document.getElementById ("sketchup").value = "4";
+ 
+document.getElementById ("html").value = "3";
+     
+document.getElementById ("css").value = "2";
+      
+document.getElementById ("javascript").value = "2";
+        
+}
+
